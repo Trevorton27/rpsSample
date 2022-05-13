2750f431d2ae29d651636ce8ff02a5c5a1a25e45
Enshinyan <emeka.enshinyan@gmail.com>
2022-05-13 13:17:31 -0700

    add: user and computer values

--- App.js
+++ App.js
@@ -1,15 +1,20 @@
-var form = document.getElementById("addForm");
-var p = document.getElementById("p1");
+var userRock = document.getElementById("rockBtn").addEventListener('click', playGame);
+var userPaper = document.getElementById("paperBtn").addEventListener('click', playGame);
+var userScissors = document.getElementById("scissorsBtn").addEventListener('click', playGame);
+var compRock = document.getElementById("rockBtn").addEventListener('click', playGame);
+var compPaper = document.getElementById("paperBtn").addEventListener('click', playGame);
+var compScissors = document.getElementById("scissorsBtn").addEventListener('click', playGame);
+var user = document.getElementById("user-choice");
 var message = "you chose rock";
 
-
-form.addEventListener("submit", playGame);
-
-function playGame(event){
-event.preventDefault();
-console.log(1);
-message = p;
-
+function playGame(user, computer){
+    userRock = 2;
+    userPaper = 3;
+    userScissors = 5;
+    compRock = 7;
+    compPaper = 11;
+    compScissors = 17;
+   
 
 
 //make an array and have program pull out a random el
