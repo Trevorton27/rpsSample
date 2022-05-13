98fef15b2745e16c010f78469e154b6850135d2e
Enshinyan <emeka.enshinyan@gmail.com>
2022-05-12 17:49:00 -0700

    edit: vars

---
+++ App.js
@@ -0,0 +1,20 @@
+var rock = document.getElementById("rock");
+var paper = document.getElementById("paper");
+var scissors = document.getElementById("scissors");
+
+var r = "rock";
+var p = "paper";
+var s = "scissors";
+
+rock.addEventListener("submit", playGame)
+
+function playGame(){
+
+//make an array and have program pull out a random el
+
+
+if (r === "rock"){
+    console.log("scissors");
+}
+
+}
