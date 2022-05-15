a4adbcf5512c9f07679a085791f53035d69aabc5
Enshinyan <emeka.enshinyan@gmail.com>
2022-05-15 12:33:46 -0700

    edit: create functions and working buttons

--- App.js
+++ App.js
@@ -1,47 +1,77 @@
-const userRock = 2;
-const userPaper = 3;
-const userScissors = 5;
-const compRock = 7;
-const compPaper = 11;
-const compScissors = 17;
+const rock = document.getElementById("rockBtn");
+const rockPressed = rock.addEventListener('click', playRock);
+const paper = document.getElementById("paperBtn");
+const paperPressed = paper.addEventListener('click', playPaper);
+const scissors = document.getElementById("scissorsBtn");
+const scissorsPressed = scissors.addEventListener('click', playScissors);
 
-const rock = document.getElementById("rockBtn").addEventListener('click', playGame());
-const paper = document.getElementById("paperBtn").addEventListener('click', playGame());
-const scissors = document.getElementById("scissorsBtn").addEventListener('click', playGame());
-const user = document.getElementById("user-choice");
+const arr = ["Rock", "Paper", "Scissors"];
+let selection = arr.sort(() => 0.5 - Math.random())[0];
 
+let result = document.getElementById("winner");
 
-const rockRock = userRock - compRock; //-5
-const rockPaper = userRock - compPaper; // -9
-const rockScissors = userRock - compScissors; //-15
-const paperRock = userRock - compScissors; // -4
-const paperPaper = userPaper - compPaper; // -8
-const paperScissors = userPaper - compScissors; // -14
-const scissorsRock = userScissors - compRock; // -2
-const scissorsPaper = userScissors - compPaper; // -6
-const scissorsScissors = userScissors - compScissors; // -12
+let userScore = 0, compScore = 0;
+let playerOne = document.getElementById("user-score");
+let playerTwo = document.getElementById("computer-score");
 
-const arr = [rock, paper, scissors];
-//randomizing the computer choice
-let compChoice = arr.sort(() => 0.5 - Math.random())[0];
-//let item = items.sort(() => 0.5 - Math.random())[0];
-function playGame(){
+function computerPlays(){
+let computerChoice = document.getElementById("computer-choice");
+computerChoice.innerText = "Your Opponent Chose " + selection;
 
-    
-    switch (choice){
-        case rockRock: const tie = (y) => {
-            user.innerText = "you chose rock";
-            
-        }
+}
+
+function playRock(){
+    let userChoice = document.getElementById("user-choice");
+    userChoice.innerText = "You Chose Rock";
+    computerPlays();
+    if (selection === "Rock"){
+        tie();
+    }else if (selection === "Paper"){
+        loser();
+        compScore++;
+        playerTwo.innerText = "Computer's Score: " + compScore;
+    }else{
+        winner();
+        userScore++
+        playerOne.innerText = "Your Score: " + userScore;
     }
     
+}
 
+function playPaper(){
+    let userChoice = document.getElementById("user-choice");
+    userChoice.innerText = "You Chose Paper";
+    computerPlays();
+    if (selection === "Rock"){
+        winner();
+    }else if (selection === "Paper"){
+        tie();
+    }else{
+        loser();
+    }
+}
 
+function playScissors(){
+    let userChoice = document.getElementById("user-choice");
+    userChoice.innerText = "You Chose Scissors";
+    computerPlays();
+    if (selection === "Rock"){
+        loser();
+    }else if (selection === "Paper"){
+        winner();
+    }else{
+        tie();
+    }
+}
 
-    
-   
+function winner(){
+    result.innerText = "You Win!";
+}
 
+function loser(){
+    result.innerText = "You Lose..."
+}
 
-//make an array and have program pull out a random el
-
+function tie(){
+    result.innerText = "*Gasp* \nIt's a Tie!"
 }
